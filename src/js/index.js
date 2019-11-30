35297342c99253a02bcb03ec0755aa3d776c5123
Raed <abdennadher.raed@gmail.com>
2019-11-30 18:52:36 +0100

    display heatmap by player by match in a given competition

---
+++ src/js/index.js
@@ -0,0 +1,82 @@
+const imageUrl = "img/field.jpg";
+
+// set the dimensions and margins of the graph
+const margin = {top: 20, right: 20, bottom: 20, left: 20},
+  width = 600 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3.select("#heatmap")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+$("svg").css({
+  backgroundColor: "green",
+  backgroundImage: "url(" + imageUrl + ")",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: width + "px " + height + "px",
+  backgroundPositionX: margin.left,
+  backgroundPositionY: margin.top
+});
+
+// const url = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/events/266310.json",
+//   playerName = "Lionel Andrés Messi Cuccittini";
+
+// $("#test-button").click(function () {
+//   updateD3(url, playerName);
+// });
+
+function updateD3(url, playerId) {
+  d3.json(url, function(data) {
+    data = data.filter(event => event.location).filter(event => event.player.id === playerId);
+
+    const maxX = 120;
+    const maxY = 80;
+
+    // Add X axis
+    const x = d3.scaleLinear()
+      .domain([0, maxX])
+      .range([0, width]);
+
+    svg.selectAll("g").remove();
+    svg.append("g")
+    // .attr("transform", "translate(0," + margin.top + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+      .domain([0, maxY])
+      .range([0, height]);
+    svg.append("g")
+      .call(d3.axisRight(y));
+
+    // Prepare a color palette
+    const color = d3.scaleLinear()
+      .domain([0, 0.25, 0.5, 0.75, 1]) // Points per square pixel.
+      .range(["rgba(0,0,255,.5)", "rgba(255,233,0,0.5)", "rgba(255,111,0,0.5)", "rgba(255,0,0,0.5)"]);
+
+    // compute the density data
+    const densityData = d3.contourDensity()
+      .x(function (d) {
+        return x(d.location[0]);
+      })
+      .y(function (d) {
+        return y(d.location[1]);
+      })
+      .size([width, height])
+      .bandwidth(12)
+      (data);
+
+    // show the shape!
+    svg.insert("g", "g")
+      .selectAll("path")
+      .data(densityData)
+      .enter().append("path")
+      .attr("d", d3.geoPath())
+      .attr("fill", function(d) { return color(d.value * 100); });
+  });
+}
